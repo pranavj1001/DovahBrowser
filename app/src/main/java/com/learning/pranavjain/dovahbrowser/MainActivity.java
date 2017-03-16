839aaaf25879ed356b73a799db1687d4ecddee59
pranavj1001 <pranavj1001@gmail.com>
2017-03-17 02:08:06 +0530

    add a webView

--- app/src/main/java/com/learning/pranavjain/dovahbrowser/MainActivity.java
+++ app/src/main/java/com/learning/pranavjain/dovahbrowser/MainActivity.java
@@ -2,12 +2,20 @@ package com.learning.pranavjain.dovahbrowser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+
+    WebView webScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webScreen = (WebView) findViewById(R.id.webScreen);
+
+        String url = "https://pranavj1001.github.io";
+
     }
 }
